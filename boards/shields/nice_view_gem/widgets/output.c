259e6653327121c374199416aa2ea0f8fc59a5b7
nihalmeti <nihalgmeti@gmail.com>
2025-01-07 00:37:35 +0530

    adjusted output and battery icon positions

--- boards/shields/nice_view_gem/widgets/output.c
+++ boards/shields/nice_view_gem/widgets/output.c
@@ -7,40 +7,39 @@ LV_IMG_DECLARE(bt_unbonded);
 LV_IMG_DECLARE(bt);
 LV_IMG_DECLARE(usb);
 
-#if !IS_ENABLED(CONFIG_ZMK_SPLIT) || IS_ENABLED(CONFIG_ZMK_SPLIT_ROLE_CENTRAL)
+
 static void draw_usb_connected(lv_obj_t *canvas) {
     lv_draw_img_dsc_t img_dsc;
     lv_draw_img_dsc_init(&img_dsc);
 
-    lv_canvas_draw_img(canvas, 45, 2, &usb, &img_dsc);
+    lv_canvas_draw_img(canvas, 45, 17, &usb, &img_dsc);
 }
 
 static void draw_ble_unbonded(lv_obj_t *canvas) {
     lv_draw_img_dsc_t img_dsc;
     lv_draw_img_dsc_init(&img_dsc);
 
-    lv_canvas_draw_img(canvas, 44, 0, &bt_unbonded, &img_dsc);
+    lv_canvas_draw_img(canvas, 44, 15, &bt_unbonded, &img_dsc);
 }
-#endif
 
 static void draw_ble_disconnected(lv_obj_t *canvas) {
     lv_draw_img_dsc_t img_dsc;
     lv_draw_img_dsc_init(&img_dsc);
 
-    lv_canvas_draw_img(canvas, 49, 10, &bt_no_signal, &img_dsc);
+    lv_canvas_draw_img(canvas, 49, 15, &bt_no_signal, &img_dsc);
 }
 
 static void draw_ble_connected(lv_obj_t *canvas) {
     lv_draw_img_dsc_t img_dsc;
     lv_draw_img_dsc_init(&img_dsc);
 
-    lv_canvas_draw_img(canvas, 49, 10, &bt, &img_dsc);
+    lv_canvas_draw_img(canvas, 49, 15, &bt, &img_dsc);
 }
 
 void draw_output_status(lv_obj_t *canvas, const struct status_state *state) {
     lv_draw_rect_dsc_t rect_white_dsc;
     init_rect_dsc(&rect_white_dsc, LVGL_FOREGROUND);
-    lv_canvas_draw_rect(canvas, 43, 10, 24, 15, &rect_white_dsc);
+    lv_canvas_draw_rect(canvas, 43, 15, 24, 15, &rect_white_dsc);
 
 #if !IS_ENABLED(CONFIG_ZMK_SPLIT) || IS_ENABLED(CONFIG_ZMK_SPLIT_ROLE_CENTRAL)
     switch (state->selected_endpoint.transport) {
