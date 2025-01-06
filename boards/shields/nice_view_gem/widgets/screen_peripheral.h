d36ff831645b9f9740d7150c797cbf8c755ed4f6
nihalmeti <nihalgmeti@gmail.com>
2025-01-06 12:09:23 +0530

    horizontal

--- boards/shields/nice_view_gem/widgets/screen_peripheral.h
+++ boards/shields/nice_view_gem/widgets/screen_peripheral.h
@@ -1,15 +1,15 @@
-#pragma once
+// #pragma once
 
-#include <lvgl.h>
-#include <zephyr/kernel.h>
-#include "util.h"
+// #include <lvgl.h>
+// #include <zephyr/kernel.h>
+// #include "util.h"
 
-struct zmk_widget_screen {
-    sys_snode_t node;
-    lv_obj_t *obj;
-    lv_color_t cbuf[BUFFER_SIZE * BUFFER_SIZE];
-    struct status_state state;
-};
+// struct zmk_widget_screen {
+//     sys_snode_t node;
+//     lv_obj_t *obj;
+//     lv_color_t cbuf[BUFFER_SIZE * BUFFER_SIZE];
+//     struct status_state state;
+// };
 
-int zmk_widget_screen_init(struct zmk_widget_screen *widget, lv_obj_t *parent);
-lv_obj_t *zmk_widget_screen_obj(struct zmk_widget_screen *widget);
+// int zmk_widget_screen_init(struct zmk_widget_screen *widget, lv_obj_t *parent);
+// lv_obj_t *zmk_widget_screen_obj(struct zmk_widget_screen *widget);
