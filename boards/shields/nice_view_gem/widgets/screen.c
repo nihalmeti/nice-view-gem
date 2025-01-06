dddabc7537387ef3a35854ebfc5f31d2afbcb038
nihalmeti <nihalgmeti@gmail.com>
2025-01-06 22:53:30 +0530

    horizontal display

--- boards/shields/nice_view_gem/widgets/screen.c
+++ boards/shields/nice_view_gem/widgets/screen.c
@@ -255,7 +255,7 @@ int zmk_widget_screen_init(struct zmk_widget_screen *widget, lv_obj_t *parent) {
     lv_obj_t *top = lv_canvas_create(widget->obj);
     lv_obj_align(top, LV_ALIGN_TOP_RIGHT, 0, 0);
     lv_canvas_set_buffer(top, widget->cbuf, BUFFER_SIZE, BUFFER_SIZE, LV_IMG_CF_TRUE_COLOR);
-
+    draw_animation(widget->obj);
     // lv_obj_t *middle = lv_canvas_create(widget->obj);
     // lv_obj_align(middle, LV_ALIGN_TOP_RIGHT, BUFFER_OFFSET_MIDDLE, 0);
     // lv_canvas_set_buffer(middle, widget->cbuf2, BUFFER_SIZE, BUFFER_SIZE, LV_IMG_CF_TRUE_COLOR);
@@ -269,6 +269,8 @@ int zmk_widget_screen_init(struct zmk_widget_screen *widget, lv_obj_t *parent) {
     // widget_layer_status_init();
     widget_output_status_init();
     // widget_wpm_status_init();
+
+
 
     return 0;
 }
