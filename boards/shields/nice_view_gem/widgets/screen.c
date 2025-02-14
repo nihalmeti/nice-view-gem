c00b4ca73a2ebc2fdbd411dea958b081766eaaee
nihalmeti <nihalgmeti@gmail.com>
2025-02-15 00:12:25 +0530

    horizontal nice view

--- boards/shields/nice_view_gem/widgets/screen.c
+++ boards/shields/nice_view_gem/widgets/screen.c
@@ -201,17 +201,17 @@ static void draw_middle(lv_obj_t *widget, lv_color_t cbuf[], const struct status
     rotate_canvas(canvas, cbuf);
 }
 
-static void draw_bottom(lv_obj_t *widget, lv_color_t cbuf[], const struct status_state *state) {
-    lv_obj_t *canvas = lv_obj_get_child(widget, 2);
-    fill_background(canvas);
+// static void draw_bottom(lv_obj_t *widget, lv_color_t cbuf[], const struct status_state *state) {
+//     lv_obj_t *canvas = lv_obj_get_child(widget, 2);
+//     fill_background(canvas);
 
-    // Draw widgets
-    draw_profile_status(canvas, state);
-    draw_layer_status(canvas, state);
+//     // Draw widgets
+//     draw_profile_status(canvas, state);
+//     draw_layer_status(canvas, state);
 
-    // Rotate for horizontal display
-    rotate_canvas(canvas, cbuf);
-}
+//     // Rotate for horizontal display
+//     rotate_canvas(canvas, cbuf);
+// }
 
 /**
  * Battery status
@@ -255,27 +255,27 @@ ZMK_SUBSCRIPTION(widget_battery_status, zmk_usb_conn_state_changed);
  * Layer status
  **/
 
-static void set_layer_status(struct zmk_widget_screen *widget, struct layer_status_state state) {
-    widget->state.layer_index = state.index;
-    widget->state.layer_label = state.label;
+// static void set_layer_status(struct zmk_widget_screen *widget, struct layer_status_state state) {
+//     widget->state.layer_index = state.index;
+//     widget->state.layer_label = state.label;
 
-    draw_bottom(widget->obj, widget->cbuf3, &widget->state);
-}
+//     draw_bottom(widget->obj, widget->cbuf3, &widget->state);
+// }
 
-static void layer_status_update_cb(struct layer_status_state state) {
-    struct zmk_widget_screen *widget;
-    SYS_SLIST_FOR_EACH_CONTAINER(&widgets, widget, node) { set_layer_status(widget, state); }
-}
+// static void layer_status_update_cb(struct layer_status_state state) {
+//     struct zmk_widget_screen *widget;
+//     SYS_SLIST_FOR_EACH_CONTAINER(&widgets, widget, node) { set_layer_status(widget, state); }
+// }
 
-static struct layer_status_state layer_status_get_state(const zmk_event_t *eh) {
-    uint8_t index = zmk_keymap_highest_layer_active();
-    return (struct layer_status_state){.index = index, .label = zmk_keymap_layer_name(index)};
-}
+// static struct layer_status_state layer_status_get_state(const zmk_event_t *eh) {
+//     uint8_t index = zmk_keymap_highest_layer_active();
+//     return (struct layer_status_state){.index = index, .label = zmk_keymap_layer_name(index)};
+// }
 
-ZMK_DISPLAY_WIDGET_LISTENER(widget_layer_status, struct layer_status_state, layer_status_update_cb,
-                            layer_status_get_state)
+// ZMK_DISPLAY_WIDGET_LISTENER(widget_layer_status, struct layer_status_state, layer_status_update_cb,
+//                             layer_status_get_state)
 
-ZMK_SUBSCRIPTION(widget_layer_status, zmk_layer_state_changed);
+// ZMK_SUBSCRIPTION(widget_layer_status, zmk_layer_state_changed);
 
 /**
  * Output status
@@ -359,13 +359,13 @@ int zmk_widget_screen_init(struct zmk_widget_screen *widget, lv_obj_t *parent) {
     lv_obj_align(middle, LV_ALIGN_TOP_RIGHT, BUFFER_OFFSET_MIDDLE, 0);
     lv_canvas_set_buffer(middle, widget->cbuf2, BUFFER_SIZE, BUFFER_SIZE, LV_IMG_CF_TRUE_COLOR);
 
-    lv_obj_t *bottom = lv_canvas_create(widget->obj);
-    lv_obj_align(bottom, LV_ALIGN_TOP_RIGHT, BUFFER_OFFSET_BOTTOM, 0);
-    lv_canvas_set_buffer(bottom, widget->cbuf3, BUFFER_SIZE, BUFFER_SIZE, LV_IMG_CF_TRUE_COLOR);
+    // lv_obj_t *bottom = lv_canvas_create(widget->obj);
+    // lv_obj_align(bottom, LV_ALIGN_TOP_RIGHT, BUFFER_OFFSET_BOTTOM, 0);
+    // lv_canvas_set_buffer(bottom, widget->cbuf3, BUFFER_SIZE, BUFFER_SIZE, LV_IMG_CF_TRUE_COLOR);
 
     sys_slist_append(&widgets, &widget->node);
     widget_battery_status_init();
-    widget_layer_status_init();
+    // widget_layer_status_init();
     widget_output_status_init();
     widget_wpm_status_init();
 
--- boards/shields/nice_view_gem/widgets/wpm.c
+++ boards/shields/nice_view_gem/widgets/wpm.c
@@ -3,54 +3,9 @@
 #include "wpm.h"
 #include "../assets/custom_fonts.h"
 
-LV_IMG_DECLARE(gauge);
+
 LV_IMG_DECLARE(grid);
 
-static void draw_gauge(lv_obj_t *canvas, const struct status_state *state) {
-    lv_draw_img_dsc_t img_dsc;
-    lv_draw_img_dsc_init(&img_dsc);
-
-    lv_canvas_draw_img(canvas, 16, 44 + BUFFER_OFFSET_MIDDLE, &gauge, &img_dsc);
-}
-
-static void draw_needle(lv_obj_t *canvas, const struct status_state *state) {
-    lv_draw_line_dsc_t line_dsc;
-    init_line_dsc(&line_dsc, LVGL_FOREGROUND, 1);
-
-    int centerX = 33;
-    int centerY = 67 + BUFFER_OFFSET_MIDDLE;
-    int offset = 13;
-    int value = state->wpm[9];
-
-#if IS_ENABLED(CONFIG_NICE_VIEW_GEM_WPM_FIXED_RANGE)
-    float max = CONFIG_NICE_VIEW_GEM_WPM_FIXED_RANGE_MAX;
-#else
-    float max = 0;
-    for (int i = 0; i < 10; i++) {
-        if (state->wpm[i] > max) {
-            max = state->wpm[i];
-        }
-    }
-#endif
-    if (max == 0)
-        max = 100;
-    if (value < 0)
-        value = 0;
-    if (value > max)
-        value = max;
-
-    float radius = 25.45585;
-    float angleDeg = 225 + ((float)value / max) * 90;
-    float angleRad = angleDeg * (3.14159 / 180.0f);
-
-    int needleStartX = centerX + (int)(offset * cos(angleRad));
-    int needleStartY = centerY + (int)(offset * sin(angleRad));
-    int needleEndX = centerX + (int)(radius * cos(angleRad));
-    int needleEndY = centerY + (int)(radius * sin(angleRad));
-
-    lv_point_t points[2] = {{needleStartX, needleStartY}, {needleEndX, needleEndY}};
-    lv_canvas_draw_line(canvas, points, 2, &line_dsc);
-}
 
 static void draw_grid(lv_obj_t *canvas) {
     lv_draw_img_dsc_t img_dsc;
@@ -123,8 +78,6 @@ static void draw_label(lv_obj_t *canvas, const struct status_state *state) {
 }
 
 void draw_wpm_status(lv_obj_t *canvas, const struct status_state *state) {
-    draw_gauge(canvas, state);
-    draw_needle(canvas, state);
     draw_grid(canvas);
     draw_graph(canvas, state);
     draw_label(canvas, state);
