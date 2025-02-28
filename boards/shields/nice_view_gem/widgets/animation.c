adf838004412a6449332e3ea7e339039f7047aed
nihalmeti <nihalgmeti@gmail.com>
2025-03-01 02:16:28 +0530

    simon_cat

--- boards/shields/nice_view_gem/widgets/animation.c
+++ boards/shields/nice_view_gem/widgets/animation.c
@@ -339,7 +339,7 @@ void draw_animation(lv_obj_t *canvas) {
     lv_obj_t *art = lv_animimg_create(canvas);
     lv_obj_center(art);
 
-    lv_animimg_set_src(art, (const void **)anim_imgs, 30);   //change 16 to the number of images in the array
+    lv_animimg_set_src(art, (const void **)anim_imgs, 51);   //change 16 to the number of images in the array
     lv_animimg_set_duration(art, CONFIG_NICE_VIEW_GEM_ANIMATION_MS);
     lv_animimg_set_repeat_count(art, LV_ANIM_REPEAT_INFINITE);
     lv_animimg_start(art);
